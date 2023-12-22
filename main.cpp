81ed3ce32ee4ea33397def5cac7ea6f36a9edaa8
appledog <appledog@tju.edu.cn>
2023-12-22 22:53:36 +0800

    add a class Shader

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <math.h>
 #include "init.h"
 #include "shader.h"
 
@@ -12,13 +13,6 @@ float vertexs[] {           // 记录六个顶点 分别对应两个三角形
     -0.5f, -0.5f, 0.0f,
      0.5f, -0.5f, 0.0f 
 };
-
-const char* vertexShaderSource =    // 顶点着色器源码
-"#version 330 core \n"
-"layout (location = 0) in vec3 aPos; \n"
-"void main() { \n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); \n"
-"} \0";
 
 const char* fragmentShaderSource1 =  //第一个片段着色器源码
 "#version 330 core \n"
@@ -65,19 +59,23 @@ int main() {
     glEnableVertexAttribArray(0);               //启用第0号顶点属性
 
     
-    unsigned int ShaderPropram1 = shader_propram(vertexShaderSource, fragmentShaderSource1);              //创建第一个着色器程序（绿色）
-    unsigned int ShaderPropram2 = shader_propram(vertexShaderSource, fragmentShaderSource2);              //创建第二个着色器程序（蓝色）
-    
+    Shader myshader1("../shaderSource/vs.txt", "../shaderSource/fs_1.txt");
+    Shader myshader2("../shaderSource/vs.txt", "../shaderSource/fs_2.txt");
+
     while(!glfwWindowShouldClose(mywindow)) {   // 渲染循环
         processInput(mywindow);
 
-        glClearColor(1.0f, 0.75f, 0.79f, 1.0f); // 设置清屏颜色
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // 设置清屏颜色
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(ShaderPropram1);            // 使用该着色器程序
+        myshader1.use();
+        float timeValue = glfwGetTime();
+        float greenvalue = (sin(timeValue) / 2.0f) + 0.5f;
+        myshader1.setFloat("changeColor", greenvalue);
         glBindVertexArray(VAO[0]);              // 使用该VAO绑定的顶点属性与VBO
         glDrawArrays(GL_TRIANGLES, 0, 3);       // 从第一个点到第三个点绘制第一个三角形
-        glUseProgram(ShaderPropram2);
+        
+        myshader2.use();
         glBindVertexArray(VAO[1]);
         glDrawArrays(GL_TRIANGLES, 3, 3);       // 从第三个点到第六个点绘制第二个三角形
 
